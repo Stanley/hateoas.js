370653687f9dc3fd5203ed3f51bb3c16914317ce
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2012-06-20 11:58:18 +0200

    + recursive definition of methods on the result object

--- specs/basic.spec.js
+++ specs/basic.spec.js
@@ -24,23 +24,6 @@ describe('HATEOAS.js basics', function(){
         }
       })))
 
-      this.once('/books', Server.response(JSON.stringify({
-        collection: {
-          items: [
-            {id: 'tfotr', name: 'The Fellowship of the Ring', author: 'J. R. R. Tolkien'},
-            {id: 'ttt', name: 'The Two Towers', author: 'J. R. R. Tolkien'},
-            {id: 'trotk', name: 'The Return of the King', author: 'J. R. R. Tolkien'}
-          ]
-        }
-      })))
-
-      this.once('/books/tfotr', Server.response(JSON.stringify({
-        collection: {
-          items: [
-            {id: 'tfotr', name: 'The Fellowship of the Ring', author: 'J. R. R. Tolkien'}
-          ]
-        }
-      })))
       server = this
     })
 
@@ -56,36 +39,91 @@ describe('HATEOAS.js basics', function(){
     })
 
     it('should query given collection', function(){
+
+      server.once('/books', Server.response(JSON.stringify({
+        collection: {
+          items: [
+            {data: {id: 'tfotr', name: 'The Fellowship of the Ring', author: 'J. R. R. Tolkien'}},
+            {data: {id: 'ttt', name: 'The Two Towers', author: 'J. R. R. Tolkien'}},
+            {data: {id: 'trotk', name: 'The Return of the King', author: 'J. R. R. Tolkien'}}
+          ]
+        }
+      })))
+
       var result
       api.books(function(books){
-        result = JSON.parse(books)
+        result = books
       })
       
       waitsFor(function(){ return result }, 'server response', 100)
       runs(function(){
         expect(result).toEqual({collection: {
           items: [
-            {id: 'tfotr', name: 'The Fellowship of the Ring', author: 'J. R. R. Tolkien'},
-            {id: 'ttt', name: 'The Two Towers', author: 'J. R. R. Tolkien'},
-            {id: 'trotk', name: 'The Return of the King', author: 'J. R. R. Tolkien'}
+            {data: {id: 'tfotr', name: 'The Fellowship of the Ring', author: 'J. R. R. Tolkien'}},
+            {data: {id: 'ttt', name: 'The Two Towers', author: 'J. R. R. Tolkien'}},
+            {data: {id: 'trotk', name: 'The Return of the King', author: 'J. R. R. Tolkien'}}
           ]
         }})
       })
     })
 
     it('should query given item', function(){
+
+      server.once('/books/tfotr', Server.response(JSON.stringify({
+        collection: {
+          items: [
+            {
+              data: {id: 'tfotr', name: 'The Fellowship of the Ring', author: 'J. R. R. Tolkien'}
+            }
+          ]
+        }
+      })))
+
       var result
       api.books('tfotr', function(book){
-        result = JSON.parse(book)
+        result = book
       })
       
       waitsFor(function(){ return result }, 'server response', 100)
       runs(function(){
-        expect(result).toEqual({collection: {
+        expect(result.collection.items.length).toEqual(1)
+        expect(result.collection.items[0].data).toEqual(
+          {id: 'tfotr', name: 'The Fellowship of the Ring', author: 'J. R. R. Tolkien'}
+        )
+      })
+    })
+
+    it('should define methods from links recursively', function(){
+
+      server.once('/books/ttt/author', Server.response(JSON.stringify({
+        collection: {
           items: [
-            {id: 'tfotr', name: 'The Fellowship of the Ring', author: 'J. R. R. Tolkien'}
+            {id: 'jrrt', name: 'J. R. R. Tolkien'}
           ]
-        }})
+        }
+      })))
+      server.once('/books/ttt', Server.response(JSON.stringify({
+        collection: {
+          items: [
+            {
+              links: [{rel: 'author', href: 'http://localhost:3000/books/ttt/author'}]
+            }
+          ]
+        }
+      })))
+
+      var result
+      api.books('ttt', function(book){
+        book.author(function(author){
+          result = author
+        })
+      })
+
+      waitsFor(function(){ return result }, 'server response', 100)
+      runs(function(){
+        expect(result.collection.items).toEqual([
+          {id: 'jrrt', name: 'J. R. R. Tolkien'}
+        ])
       })
     })
   })
